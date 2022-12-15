1587088ef526fb9306ee651d8dcdedaab0e3d3fb
Anastasiya Molinska <a.molinska@mobidev.biz>
2022-12-15 16:25:40 +0100

    LOOPCARE-31 basic widgets and theme

---
+++ lib/core/presentation/alerting/show_app_snackbar.dart
@@ -0,0 +1,42 @@
+import 'package:flash/flash.dart';
+import 'package:flutter/material.dart';
+
+import '../themes/themes.dart';
+
+void showAppSnackBar({
+  required BuildContext context,
+  required String text,
+  Color? background,
+}) {
+  final size = MediaQuery.of(context).size;
+
+  showFlash(
+    context: context,
+    duration: const Duration(
+      seconds: 5,
+    ),
+    builder: (context, FlashController controller) {
+      return Flash(
+        controller: controller,
+        alignment: Alignment.topCenter,
+        backgroundColor: background ?? AppColors.greenMid,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(8.0),
+        ),
+        child: SizedBox(
+          width: size.width * 0.8718,
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Text(
+              text,
+              style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                    color: AppColors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
---
+++ lib/core/presentation/icon_images/app_icons.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AppIcons {
+  static const String iconsFilePath = 'assets/icons';
+
+  static const AssetImage iconOpenEye =
+      AssetImage('$iconsFilePath/icon_open_eye.png');
+
+  AppIcons._();
+}
