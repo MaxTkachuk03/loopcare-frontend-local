03fb1922584d8803294908144006735f98740d8d
d.yavdoshenko <d.yavdoshenko@mobidev.biz>
2023-01-05 15:16:48 +0000

    LOOPCARE-53: Forgot Password page

--- lib/features/intro/presentation/widgets/intro_bottom.dart
+++ lib/features/intro/presentation/widgets/intro_bottom.dart
@@ -56,6 +56,6 @@ class IntroBottom extends StatelessWidget {
   }
 
   void _onLoginTap(BuildContext context) {
-    context.router.pushNamed(AppRoutes.physicalCheckFailed);
+    context.router.pushNamed(AppRoutes.forgotPassword);
   }
 }
