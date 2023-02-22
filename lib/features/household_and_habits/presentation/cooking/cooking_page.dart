d57fa7bd6bff063223d40e26af272447226a6f36
Anastasiya Molinska <a.molinska@mobidev.biz>
2023-02-22 11:37:38 +0100

    LOOPCARE-283 cooking

--- lib/features/household_and_habits/presentation/cooking/cooking_page.dart
+++ lib/features/household_and_habits/presentation/cooking/cooking_page.dart
@@ -1,30 +1,28 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:loopcare_frontend/core/presentation/localization/localized_texts.dart';
-import 'package:loopcare_frontend/core/presentation/routes/app_router.dart';
+import 'package:loopcare_frontend/features/household_and_habits/presentation/cooking/widgets/cooking_chips.dart';
 import 'package:loopcare_frontend/features/household_and_habits/presentation/household_and_habits_question.dart';
-import 'package:loopcare_frontend/features/household_and_habits/presentation/share_meal_with/widgets/share_meal_with_chips.dart';
 
-class ShareMealPage extends StatelessWidget {
-  const ShareMealPage({Key? key}) : super(key: key);
+class CookingPage extends StatelessWidget {
+  const CookingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return HouseholdAndHabitsQuestion(
-      subtitle: '1 ${LocalizedTexts.of.tr()} 6',
+      subtitle: '2 ${LocalizedTexts.of.tr()} 6',
       question: Text(
-        LocalizedTexts.shareMealWithQuestion.tr(),
+        LocalizedTexts.cookingQuestion.tr(),
         style: Theme.of(context).textTheme.bodyText1?.copyWith(
           fontWeight: FontWeight.w600,
         ),
       ),
-      questionList: const ShareMealWithChips(),
+      questionList: const CookingChips(),
       onNextPressed: () => _onNextPressed(context),
     );
   }
 
   void _onNextPressed(BuildContext context) {
-    context.router.pushNamed(AppRoutes.cooking);
+
   }
 }
