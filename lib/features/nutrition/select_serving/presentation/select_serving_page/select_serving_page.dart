6ae6aa3e7a1b36f6562691a53b7b16e7be2a3f25
Artur Serdiuk <a.serdiuk@mobidev.biz>
2023-03-14 18:26:13 +0100

    [Feature] LOOPCARE - 459 UI food item serving screen Changes for favourite filters bottom sheet

--- lib/features/nutrition/select_serving/presentation/select_serving_page/select_serving_page.dart
+++ lib/features/nutrition/select_serving/presentation/select_serving_page/select_serving_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:loopcare_frontend/core/presentation/alerting/modal_bottom_sheet.dart';
 import 'package:loopcare_frontend/core/presentation/localization/localized_texts.dart';
 import 'package:loopcare_frontend/core/presentation/themes/themes.dart';
+import 'package:loopcare_frontend/core/presentation/widgets/back_button_hexagon.dart';
 import 'package:loopcare_frontend/features/nutrition/nutrition_instructions/domain/dialog_filter.dart';
 import 'package:loopcare_frontend/features/nutrition/select_serving/presentation/widgets/favourite_btn/favourite_btn.dart';
 import 'package:loopcare_frontend/features/nutrition/select_serving/presentation/widgets/servings_list/servings_list.dart';
@@ -19,7 +20,7 @@ class _SelectServingPageState extends State<SelectServingPage> {
   @override
   void initState() {
     // TODO get value from the foodItem
-    _isFavourite = true;
+    _isFavourite = false;
     // TODO: make a request to get ServingTypes for the foodItem
     super.initState();
   }
@@ -28,6 +29,7 @@ class _SelectServingPageState extends State<SelectServingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: const BackButtonHexagon(),
         titleTextStyle: Theme.of(context).textTheme.headline5?.copyWith(
               color: AppColors.white,
             ),
@@ -48,12 +50,15 @@ class _SelectServingPageState extends State<SelectServingPage> {
     // TODO send request to the server to add foodItem to the favourites
   }
 
+  _onFavouriteFilterPressed(bool value, int id) {}
+
   _onFavouritePressed() {
     ModalBottomSheet.filterDialog(
       context: context,
       title: LocalizedTexts.addAsFavourite.translation,
       subtitle: 'Chicken roasted or grilled serving: 1 piece (150 g)',
       onConfirmed: _onAddAsFavouriteConfirmedPressed,
+      onChanged: _onFavouriteFilterPressed,
       list: const [
         DialogFilter(id: 0, name: 'Breakfast', selected: false),
         DialogFilter(id: 1, name: 'Lunch', selected: false),
@@ -61,7 +66,6 @@ class _SelectServingPageState extends State<SelectServingPage> {
         DialogFilter(id: 3, name: 'Inbetweens & snacks', selected: false),
         DialogFilter(id: 4, name: 'Drinks ', selected: false),
       ],
-      onChanged: (bool value, int id) {},
     );
   }
 }
