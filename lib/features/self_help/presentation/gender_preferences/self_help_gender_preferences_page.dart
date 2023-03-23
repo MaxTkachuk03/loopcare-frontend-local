c1458fb603f5cb52b082aa53469918bc74e69405
d.yavdoshenko <d.yavdoshenko@mobidev.biz>
2023-03-23 15:36:49 +0000

    LOOPCARE-485: Fix No selection is allowed for Self-Help survey

--- lib/features/self_help/presentation/gender_preferences/self_help_gender_preferences_page.dart
+++ lib/features/self_help/presentation/gender_preferences/self_help_gender_preferences_page.dart
@@ -1,32 +1,55 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loopcare_frontend/core/presentation/localization/localized_texts.dart';
 import 'package:loopcare_frontend/core/presentation/routes/app_router.dart';
 import 'package:loopcare_frontend/core/presentation/widgets/questions_wizard.dart';
+import 'package:loopcare_frontend/features/self_help/application/self_help_bloc.dart';
 import 'package:loopcare_frontend/features/self_help/presentation/gender_preferences/self_help_gender_preferences_chips.dart';
 
-class SelfHelpGenderPreferencesPage extends StatelessWidget {
+class SelfHelpGenderPreferencesPage extends StatefulWidget {
   const SelfHelpGenderPreferencesPage({Key? key}) : super(key: key);
 
   @override
+  State<SelfHelpGenderPreferencesPage> createState() =>
+      _SelfHelpGenderPreferencesPageState();
+}
+
+class _SelfHelpGenderPreferencesPageState
+    extends State<SelfHelpGenderPreferencesPage> {
+  bool _isEnabled = false;
+
+  void _onSelectedDHandler(bool isSelected) => _isEnabled = isSelected;
+
+  @override
   Widget build(BuildContext context) {
-    return QuestionsWizard(
-      stepTitle: LocalizedTexts.selfHelpTitle.tr(),
-      currentStep:
-          LocalizedTexts.currentStep.translation.translateWithNamedArgs({
-        'currentStep': "1",
-        'totalSteps': "1",
-      }),
-      question: Text(
-        LocalizedTexts.selfHelpGenderPreferencesPage1Header.tr(),
-        style: Theme.of(context).textTheme.bodyText1?.copyWith(
-              fontWeight: FontWeight.w600,
-            ),
-      ),
-      questionList: const SelfHelpGenderPreferencesChips(),
-      onNextPressed: () {
-        context.router.pushNamed(AppRoutes.selfHelpReady);
+    return BlocBuilder<SelfHelpBloc, SelfHelpState>(
+      builder: (BuildContext context, state) {
+        return QuestionsWizard(
+          stepTitle: LocalizedTexts.selfHelpTitle.tr(),
+          currentStep:
+              LocalizedTexts.currentStep.translation.translateWithNamedArgs(
+            {
+              'currentStep': "1",
+              'totalSteps': "1",
+            },
+          ),
+          question: Text(
+            LocalizedTexts.selfHelpGenderPreferencesPage1Header.tr(),
+            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                  fontWeight: FontWeight.w600,
+                ),
+          ),
+          questionList: SelfHelpGenderPreferencesChips(
+            onSelected: (bool value) {
+              _onSelectedDHandler(value);
+            },
+          ),
+          onNextPressed: () => _isEnabled || state.isCompleted
+              ? context.router.pushNamed(AppRoutes.selfHelpReady)
+              : null,
+        );
       },
     );
   }
