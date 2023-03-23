20795e354a11468fd62b2418bccd597bc469f1ef
a.molinska <a.molinska@mobidev.biz>
2023-03-23 15:00:39 +0000

    Change dashboard nutrition page for testing

--- lib/features/intro/presentation/widgets/intro_bottom.dart
+++ lib/features/intro/presentation/widgets/intro_bottom.dart
@@ -56,6 +56,6 @@ class IntroBottom extends StatelessWidget {
   }
 
   void _onLoginTap(BuildContext context) {
-    context.router.pushNamed(AppRoutes.nutritionDashboard);
+    context.router.pushNamed(AppRoutes.login);
   }
 }
