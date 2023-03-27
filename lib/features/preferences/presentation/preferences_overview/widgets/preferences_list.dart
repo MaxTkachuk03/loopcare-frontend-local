8da099ac87134c5adc6d77faca429eedbc395849
Artur Serdiuk <a.serdiuk@mobidev.biz>
2023-03-27 07:39:31 +0200

    [Fix] removed household pref

--- lib/features/preferences/presentation/preferences_overview/widgets/preferences_list.dart
+++ lib/features/preferences/presentation/preferences_overview/widgets/preferences_list.dart
@@ -47,21 +47,6 @@ class _PreferencesListState extends State<PreferencesList> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Column(
-          children: [
-            PreferencesListItem(
-              item: Pref(
-                title: 'Food temptations',
-                completionTime: '10 ${LocalizedTexts.minutes.tr()}',
-                isCompleted: false,
-                imagePath: AppImages.preferencesDiabetes,
-              ),
-              imageOverlayColor: AppColors.orange.withOpacity(0.8),
-              routePath: AppRoutes.householdIntro,
-            ),
-            const SizedBox(height: 8.0),
-          ],
-        ),
         BlocBuilder<YouAndFoodBloc, YouAndFoodState>(
             builder: (BuildContext context, state) {
           return Column(
@@ -80,36 +65,6 @@ class _PreferencesListState extends State<PreferencesList> {
             ],
           );
         }),
-        // Column(
-        //   children: [
-        //     PreferencesListItem(
-        //       item: Pref(
-        //         title: 'Food temptations',
-        //         completionTime: '10 ${LocalizedTexts.minutes.tr()}',
-        //         isCompleted: false,
-        //         imagePath: AppImages.preferencesDiabetes,
-        //       ),
-        //       imageOverlayColor: AppColors.greenLight.withOpacity(0.8),
-        //       routePath: AppRoutes.householdIntro,
-        //     ),
-        //     const SizedBox(height: 8.0),
-        //   ],
-        // ),
-        // Column(
-        //   children: [
-        //     PreferencesListItem(
-        //       item: Pref(
-        //         title: LocalizedTexts.householdAndEatingHabits.tr(),
-        //         completionTime: '10 ${LocalizedTexts.minutes.tr()}',
-        //         isCompleted: false,
-        //         imagePath: AppImages.preferencesDiabetes,
-        //       ),
-        //       imageOverlayColor: AppColors.purple.withOpacity(0.8),
-        //       routePath: AppRoutes.householdIntro,
-        //     ),
-        //     const SizedBox(height: 8.0),
-        //   ],
-        // ),
         BlocBuilder<DiabetesBloc, DiabetesState>(
             builder: (BuildContext context, state) {
           return Column(
