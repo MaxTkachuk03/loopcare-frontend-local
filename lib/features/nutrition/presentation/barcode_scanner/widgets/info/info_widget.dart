f36db4e5225cb6c282e2ea82656bbb7d4f7c0d48
d.yavdoshenko <d.yavdoshenko@mobidev.biz>
2023-03-28 06:20:25 +0000

    LOOPCARE-519: Add reseting data for start scanning

--- lib/features/nutrition/presentation/barcode_scanner/widgets/info/info_widget.dart
+++ lib/features/nutrition/presentation/barcode_scanner/widgets/info/info_widget.dart
@@ -73,6 +73,23 @@ class _QRCodeInfoWidgetState extends State<QRCodeInfoWidget> {
               ),
             );
           },
+          loading: (_) {
+            return Container(
+              height: 400,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25.0),
+                  topRight: Radius.circular(25.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 44, right: 44, bottom: 44, top: 16),
+                child: Container(),
+              ),
+            );
+          },
         );
       },
     );
