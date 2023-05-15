8b424a78fcd2e1c2350b6ef59f9a520c0bb3a968
d.yavdoshenko <d.yavdoshenko@mobidev.biz>
2023-05-15 12:28:48 +0000

    LOOPCARE-792: Update navigation for hexagon plus button

--- lib/features/nutrition/presentation/widgets/plus_button_hexagon/plus_button_hexagon.dart
+++ lib/features/nutrition/presentation/widgets/plus_button_hexagon/plus_button_hexagon.dart
@@ -1,9 +1,16 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loopcare_frontend/core/presentation/icon_images/app_icons.dart';
 import 'package:loopcare_frontend/core/presentation/routes/app_router.dart';
+import 'package:loopcare_frontend/core/presentation/routes/app_router.gr.dart';
 import 'package:loopcare_frontend/core/presentation/themes/themes.dart';
 import 'package:loopcare_frontend/core/presentation/widgets/hexagon.dart';
+import 'package:loopcare_frontend/features/nutrition/application/meals/dto/add_food_item_to_meal_body.dart';
+import 'package:loopcare_frontend/features/nutrition/application/meals/meals_bloc.dart';
+import 'package:loopcare_frontend/features/nutrition/application/search/dto/search_item.dart';
+import 'package:loopcare_frontend/features/nutrition/application/search/dto/search_item_types.dart';
+import 'package:loopcare_frontend/features/nutrition/application/search/search_bloc.dart';
 
 class PlusButtonHexagon extends StatelessWidget {
   const PlusButtonHexagon({Key? key}) : super(key: key);
@@ -24,11 +31,67 @@ class PlusButtonHexagon extends StatelessWidget {
                 color: AppColors.blueMid,
                 size: 18,
               ),
-              onPressed: () => context.router.pushNamed(AppRoutes.search),
+              onPressed: () => _onSearchTap(context),
             ),
           ),
         )
       ],
+    );
+  }
+
+  _onSearchTap(BuildContext context) {
+    context.router.push(
+      SearchRoute(
+        onItemTap: (SearchItem item) {
+          final mealBloc = context.read<MealsBloc>();
+          final mealId = mealBloc.state.getCurrentMealId;
+
+          if (mealId == null) return;
+
+          context.read<SearchBloc>().add(
+                SearchEvent.addSearchResult(item.name),
+              );
+
+          if (item.type == SearchItemTypes.food) {
+            context.router.push(
+              SelectServingRoute(
+                foodItemId: item.id,
+                foodItemName: item.name,
+                initialServingAmount: 1,
+                onConfirm: (double numberOfUnits, String servingId) {
+                  final mealId = mealBloc.state.getCurrentMealId;
+                  if (mealId != null) {
+                    mealBloc.add(
+                      MealsEvent.addFoodItemToMeal(
+                        mealId,
+                        item.id,
+                        AddFoodItemToMealBody(
+                          numberOfUnits: numberOfUnits,
+                          servingId: servingId,
+                        ),
+                      ),
+                    );
+                    context.router.pushNamed(AppRoutes.meal);
+                  }
+                },
+              ),
+            );
+          } else if (item.type == SearchItemTypes.recipe) {
+            context.router.push(
+              RecipeRoute(
+                id: int.parse(item.id),
+                name: item.name,
+              ),
+            );
+          } else if (item.type == SearchItemTypes.dish) {
+            context.router.push(
+              DishDetailsRoute(
+                dishId: int.parse(item.id),
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
