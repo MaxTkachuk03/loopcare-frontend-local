d898a29a4d5b229f5df0262752db8b3dd9c53233
d.yavdoshenko <d.yavdoshenko@mobidev.biz>
2023-05-22 16:43:54 +0000

    LOOPCARE-754: Update weight block on dashboard page

---
+++ lib/features/nutrition/presentation/dashboard/widgets/weight/loading_weight.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:loopcare_frontend/core/presentation/icon_images/app_icons.dart';
+import 'package:loopcare_frontend/core/presentation/localization/localized_texts.dart';
+import 'package:loopcare_frontend/core/presentation/themes/themes.dart';
+
+class LoadingWeight extends StatelessWidget {
+  const LoadingWeight({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(8),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            LocalizedTexts.logYourWeight.translation,
+            style: Theme.of(context).textTheme.headline5!.copyWith(
+                  fontFamily: ThemeConstants.bitterFontFamily,
+                  color: AppColors.greyLabel,
+                ),
+          ),
+          Text(
+            LocalizedTexts.preferableInTheMorning.translation,
+            style: Theme.of(context)
+                .textTheme
+                .caption!
+                .copyWith(color: AppColors.greyLabel),
+          )
+        ],
+      ),
+    );
+  }
+}
