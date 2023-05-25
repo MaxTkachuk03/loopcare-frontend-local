01f8354a06d8122e77563d30c02a1a30dd2325c3
Anastasiya Molinska <a.molinska@mobidev.biz>
2023-05-25 10:05:09 +0200

    LOOPCARE-781 add possibility to go to certain planned meal

--- lib/features/nutrition/presentation/dashboard/widgets/plan_meal/plan_meal.dart
+++ lib/features/nutrition/presentation/dashboard/widgets/plan_meal/plan_meal.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loopcare_frontend/core/presentation/routes/app_router.dart';
 import 'package:loopcare_frontend/features/nutrition/application/meals/dto/meals_list_item.dart';
 import 'package:loopcare_frontend/features/physical_fitness/utils/date_time_extensions.dart';
 import 'package:loopcare_frontend/core/presentation/alerting/modal_bottom_sheet.dart';
@@ -144,45 +145,54 @@ class PlanMeal extends StatelessWidget {
                           physics: const NeverScrollableScrollPhysics(),
                           itemCount: currentPlannedMeals.length,
                           itemBuilder: (BuildContext context, index) {
-                            return Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                            return Material(
+                              child: InkWell(
+                                onTap: () => _onMealTap(
+                                    context, currentPlannedMeals[index]),
+                                child: Ink(
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Text(
-                                        "${state.getCurrentDate.isoStringWithoutTime} ${currentPlannedMeals[index].mealCategory}"
-                                            .toUpperCase(),
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headlineSmall
-                                            ?.copyWith(
-                                              fontSize: 12.0,
-                                              color: AppColors.greyLabel,
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              "${state.getCurrentDate.isoStringWithoutTime} ${currentPlannedMeals[index].mealCategory}"
+                                                  .toUpperCase(),
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .headlineSmall
+                                                  ?.copyWith(
+                                                    fontSize: 12.0,
+                                                    color: AppColors.greyLabel,
+                                                  ),
                                             ),
+                                            Text(
+                                              currentPlannedMeals[index]
+                                                  .mealItems
+                                                  .map((e) => e.name)
+                                                  .join(', '),
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .headlineSmall
+                                                  ?.copyWith(fontSize: 12.0),
+                                              maxLines: 1,
+                                              overflow: TextOverflow.ellipsis,
+                                            )
+                                          ],
+                                        ),
                                       ),
-                                      Text(
-                                        currentPlannedMeals[index]
-                                            .mealItems
-                                            .map((e) => e.name)
-                                            .join(', '),
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headlineSmall
-                                            ?.copyWith(fontSize: 12.0),
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                      )
+                                      const Image(
+                                        image: AppIcons.arrow,
+                                        color: AppColors.greyLabel,
+                                      ),
                                     ],
                                   ),
                                 ),
-                                const Image(
-                                  image: AppIcons.arrow,
-                                  color: AppColors.greyLabel,
-                                ),
-                              ],
+                              ),
                             );
                           },
                           separatorBuilder: (BuildContext context, int index) {
@@ -197,5 +207,11 @@ class PlanMeal extends StatelessWidget {
         );
       },
     );
+  }
+
+  _onMealTap(BuildContext context, MealsListItem meal) {
+    context
+      ..read<MealsBloc>().add(MealsEvent.setPlannedMeal(meal))
+      ..router.pushNamed(AppRoutes.meal);
   }
 }
