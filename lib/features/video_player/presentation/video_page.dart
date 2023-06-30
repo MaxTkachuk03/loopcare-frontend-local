9b512dbc8a4fb9530274f7df3568ec33f00261c4
Artur Serdiuk <a.serdiuk@mobidev.biz>
2023-06-30 08:26:23 +0200

    [Fix] LOOPCARE-1074 Video continues in the background after performing Back button after Staring next video by timer or Next button

--- lib/features/video_player/presentation/video_page.dart
+++ lib/features/video_player/presentation/video_page.dart
@@ -264,8 +264,6 @@ class _VideoPageState extends State<VideoPage> {
   void dispose() {
     _onlyPortraitOrientation();
 
-    _countDownController.reset();
-
     _disposeVideoController();
 
     super.dispose();
