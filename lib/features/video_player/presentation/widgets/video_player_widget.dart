abcc09eaa95718b76e8d4c735c9961e50e99b63b
Artur Serdiuk <a.serdiuk@mobidev.biz>
2023-07-03 14:23:03 +0200

    [Fix] LOOPCARE-1076 Skip button and overlay behaviour changes Hide video controlls if user user taps anywhere except buttons Skip explanation button now visible even overlay is hidden during introduction section

--- lib/features/video_player/presentation/widgets/video_player_widget.dart
+++ lib/features/video_player/presentation/widgets/video_player_widget.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:loopcare_frontend/features/physical_activities/domain/physical_program_exercise.dart';
@@ -8,7 +6,7 @@ import 'package:loopcare_frontend/features/video_player/presentation/widgets/pla
 import 'package:loopcare_frontend/features/video_player/presentation/widgets/video_block.dart';
 import 'package:video_player/video_player.dart';
 
-class VideoPlayerWidget extends StatefulWidget {
+class VideoPlayerWidget extends StatelessWidget {
   final VideoPlayerController? controller;
   final Orientation orientation;
   final PhysicalProgramExercise exercise;
@@ -35,86 +33,41 @@ class VideoPlayerWidget extends StatefulWidget {
   });
 
   @override
-  State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
-}
-
-class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
-  static const Duration _timerDuration = Duration(seconds: 5);
-  static const Duration _animationDuration = Duration(milliseconds: 350);
-
-  Timer? _timer;
-  bool _showControls = false;
-
-  // TODO add debounce to the function in case user seek with progress bar
-  void _showVideoControlsWithTimer() {
-    if (_timer != null) _timer?.cancel();
-
-    _timer = Timer.periodic(_timerDuration, (timer) {
-      setState(() {
-        _showControls = false;
-      });
-
-      timer.cancel();
-    });
-
-    setState(() {
-      _showControls = true;
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _showVideoControlsWithTimer,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          VideoBlock(controller: widget.controller, orientation: widget.orientation),
-          if (widget.controller != null)
-            ValueListenableBuilder(
-                valueListenable: widget.controller!,
-                builder: (BuildContext context, VideoPlayerValue value, child) {
-                  final videoFinished = value.isInitialized && value.position == value.duration;
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        VideoBlock(controller: controller, orientation: orientation),
+        if (controller != null)
+          ValueListenableBuilder(
+            valueListenable: controller!,
+            builder: (BuildContext context, VideoPlayerValue value, child) {
+              final videoFinished = value.isInitialized && value.position == value.duration;
 
-                  return videoFinished
-                      ? PlayerEndVideoOverlay(
-                          controller: widget.controller!,
-                          orientation: widget.orientation,
-                          exercise: widget.exercise,
-                          onVideoEnds: widget.onVideoEnds,
-                          isLastVideo: widget.isLastVideo,
-                          countDownController: widget.countDownController,
-                          programType: widget.programType,
-                          programDifficulty: widget.programDifficulty,
-                          programLength: widget.programLength,
-                        )
-                      : AnimatedOpacity(
-                          duration: _animationDuration,
-                          opacity: _showControls ? 1 : 0,
-                          child: Visibility(
-                            visible: _showControls,
-                            child: PlayerOverlay(
-                              controller: widget.controller!,
-                              orientation: widget.orientation,
-                              exercise: widget.exercise,
-                              onPrevPressed: widget.onPrevPressed,
-                              onNextPressed: widget.onVideoEnds,
-                              programType: widget.programType,
-                              programDifficulty: widget.programDifficulty,
-                              onSliderProgressChange: _showVideoControlsWithTimer,
-                            ),
-                          ),
-                        );
-                }),
-        ],
-      ),
+              return videoFinished
+                  ? PlayerEndVideoOverlay(
+                      controller: controller!,
+                      orientation: orientation,
+                      exercise: exercise,
+                      onVideoEnds: onVideoEnds,
+                      isLastVideo: isLastVideo,
+                      countDownController: countDownController,
+                      programType: programType,
+                      programDifficulty: programDifficulty,
+                      programLength: programLength,
+                    )
+                  : PlayerOverlay(
+                      controller: controller!,
+                      orientation: orientation,
+                      exercise: exercise,
+                      onPrevPressed: onPrevPressed,
+                      onNextPressed: onVideoEnds,
+                      programType: programType,
+                      programDifficulty: programDifficulty,
+                    );
+            },
+          ),
+      ],
     );
   }
 }
