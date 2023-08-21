64c287134da433a3a466fe372b2dd2d238eae86b
d.yavdoshenko <d.yavdoshenko@mobidev.biz>
2023-08-21 09:52:33 +0000

    LOOPCARE-1250: Group sessions bloc/api infrastructure

---
+++ lib/features/group_sessions/infrastructure/api_topics_service.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+import 'package:loopcare_frontend/core/infrastructure/dio_client/dio_client.dart';
+import 'package:loopcare_frontend/core/infrastructure/dio_client/parse_response.dart';
+import 'package:loopcare_frontend/core/infrastructure/dio_client/request_error.dart';
+import 'package:loopcare_frontend/features/group_sessions/appliction/dto/group_sessions_response.dart';
+import 'package:loopcare_frontend/features/group_sessions/appliction/topics_service.dart';
+
+@Injectable(as: TopicsService)
+class APITopicsService implements TopicsService {
+  DioClient client;
+
+  APITopicsService(this.client);
+
+  @override
+  Future<Either<RequestError, GroupSessionsResponse>> topics() async {
+    return client.get('/group-sessions').then(parseResponse(GroupSessionsResponse.fromJson));
+  }
+}
