64982fbdd00f50fcb0016808bc4f7d86d32c2c6d
Artur Serdiuk <a.serdiuk@mobidev.biz>
2023-08-30 17:22:32 +0200

    [Feature] LOOPCARE-1213-Participate_in_Group_Session_-_Session_prompts Added testing link

--- lib/features/video_session/presentation/widgets/session_video_container.dart
+++ lib/features/video_session/presentation/widgets/session_video_container.dart
@@ -86,11 +86,9 @@ class _SessionVideoContainerState extends State<SessionVideoContainer> with Widg
 
   void _initVideoController(String videoLink) {
     final headers = context.read<VideoPlayerBloc>().state.data.videoHttpHeaders;
-
-    _videoPlayerController = VideoPlayerController.networkUrl(
-        Uri.parse("https://d316h49i7nayz2.cloudfront.net/Lunges/index.m3u8"),
-        httpHeaders: headers,
-        videoPlayerOptions: VideoPlayerOptions(allowBackgroundPlayback: true))
+    // TODO link for testing "https://d316h49i7nayz2.cloudfront.net/Lunges/index.m3u8"
+    _videoPlayerController = VideoPlayerController.networkUrl(Uri.parse(videoLink),
+        httpHeaders: headers, videoPlayerOptions: VideoPlayerOptions(allowBackgroundPlayback: true))
       ..initialize().then((value) {
         final bool isVideoInProgress = (_currentVideoEvent?.eventStartTime ?? 0) <= widget.sessionTimer &&
             widget.sessionTimer <= (_currentVideoEvent?.eventEndTime ?? 0);
