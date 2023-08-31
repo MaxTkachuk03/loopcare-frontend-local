e152c43fc153f768507d45ec53e87ed5b0017893
Dima Yavdoshenko <d.yavdoshenko@mobidev.biz>
2023-08-31 18:46:59 +0300

    Emergency numbers screen

--- lib/core/presentation/alerting/modal_bottom_sheet.dart
+++ lib/core/presentation/alerting/modal_bottom_sheet.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loopcare_frontend/core/domain/emergency_numbers/emergency_numbers.dart';
 import 'package:loopcare_frontend/core/infrastructure/services/app_config.dart';
 import 'package:loopcare_frontend/core/presentation/html_renderer/html_renderer.dart';
 import 'package:loopcare_frontend/core/presentation/icon_images/app_icons.dart';
@@ -14,6 +15,7 @@ import 'package:loopcare_frontend/core/presentation/widgets/checkbox_blue.dart';
 import 'package:loopcare_frontend/core/presentation/widgets/main_container.dart';
 import 'package:loopcare_frontend/core/presentation/widgets/oval_bottom_border_clipper.dart';
 import 'package:loopcare_frontend/core/presentation/widgets/scrollable_container.dart';
+import 'package:loopcare_frontend/features/account/presentation/emergency_numbers/emergency_number_card.dart';
 import 'package:loopcare_frontend/features/education/application/education_lesson/education_lesson_bloc.dart';
 import 'package:loopcare_frontend/features/group_sessions/application/topics_bloc.dart';
 import 'package:loopcare_frontend/features/group_sessions/presentation/widgets/booked_session_card.dart';
@@ -1186,48 +1188,62 @@ class ModalBottomSheet {
     required void Function() onBtnPress,
   }) {
     showModalBottomSheet<void>(
-      isDismissible: false,
-      enableDrag: false,
+      isScrollControlled: true,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(24.0),
       ),
       context: context,
       builder: (BuildContext context) {
-        return Container(
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
-          height: 430,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const SizedBox(height: 54.0),
-              Align(
-                alignment: AlignmentDirectional.topCenter,
-                child: AppImages.like,
-              ),
-              const SizedBox(height: 35.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 22.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      LocalizedTexts.surveyFinishedBottomSheetTitle.tr(),
-                      style: Theme.of(context).textTheme.headlineSmall?.copyWith(color: AppColors.blueDark),
+        return FractionallySizedBox(
+          heightFactor: 0.93,
+          child: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 32.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 6.0),
+                      child: IconButton(
+                        padding: const EdgeInsets.all(0.0),
+                        onPressed: () => context.router.pop(),
+                        icon: const Icon(Icons.close, size: 16.0),
+                      ),
                     ),
-                    const SizedBox(height: 8.0),
-                    Text(
-                      LocalizedTexts.surveyFinishedBottomSheetMain.tr(),
-                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(),
+                  ),
+                  Text(
+                    LocalizedTexts.inCaseOfEmergency.tr(),
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall
+                        ?.copyWith(fontFamily: ThemeConstants.bitterFontFamily),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(LocalizedTexts.emergencySubtitle.tr(), style: Theme.of(context).textTheme.bodySmall),
+                  const SizedBox(height: 12),
+                  Expanded(
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      itemCount: emergencyNumbersList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return EmergencyNumberCard(
+                          number: emergencyNumbersList[index],
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const Divider(
+                          thickness: 2.0,
+                          height: 2.0,
+                          color: AppColors.bgGreen,
+                        );
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 27.0),
-              ElevatedButton(
-                onPressed: onBtnPress,
-                child: Text(LocalizedTexts.getStarted.translation),
-              ),
-            ],
+            ),
           ),
         );
       },
---
+++ lib/features/account/presentation/emergency_numbers/emergency_number_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:loopcare_frontend/core/domain/emergency_numbers/emergency_number_data.dart';
+import 'package:loopcare_frontend/core/presentation/themes/themes.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class EmergencyNumberCard extends StatelessWidget {
+  final EmergencyNumberData number;
+
+  const EmergencyNumberCard({
+    Key? key,
+    required this.number,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              number.title,
+              style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+            ),
+          ),
+          Expanded(
+            child: OutlinedButton(
+              onPressed: _onPressed,
+              style: Theme.of(context).outlinedButtonTheme.style?.copyWith(
+                    side: MaterialStateProperty.all(
+                      const BorderSide(
+                        width: 1.0,
+                        color: AppColors.blueDark,
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all(AppColors.blueDark),
+                  ),
+              child: Text(
+                number.btnTxt,
+                style: Theme.of(context).textTheme.bodySmall?.copyWith(color: AppColors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _onPressed() async {
+    if (number.type == EmergencyNumberType.phone) {
+      _makePhoneCall(number.number);
+    } else if (number.type == EmergencyNumberType.messenger) {
+      _launchInBrowser(number.number);
+    }
+  }
+
+  Future<void> _launchInBrowser(String url) async {
+    final Uri launchUri = Uri.parse(url);
+
+    if (!await launchUrl(
+      launchUri,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri);
+  }
+}
