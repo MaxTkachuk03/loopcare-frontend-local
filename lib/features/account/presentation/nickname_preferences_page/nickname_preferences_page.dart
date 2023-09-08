a19495409c274ad826709fb90bca300eea9eb78f
Antonina Lehka <a.lehka@mobidev.biz>
2023-09-08 11:48:49 +0300

    fix(LOOPCARE-1366) hide kayboard on preference page

--- lib/features/account/presentation/nickname_preferences_page/nickname_preferences_page.dart
+++ lib/features/account/presentation/nickname_preferences_page/nickname_preferences_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loopcare_frontend/core/presentation/localization/localized_texts.dart';
 import 'package:loopcare_frontend/core/presentation/routes/app_router.dart';
 import 'package:loopcare_frontend/core/presentation/routes/app_router.gr.dart';
+import 'package:loopcare_frontend/core/presentation/utils/keyboard_state.dart';
 import 'package:loopcare_frontend/core/presentation/widgets/main_container.dart';
 import 'package:loopcare_frontend/core/presentation/widgets/orange_button.dart';
 import 'package:loopcare_frontend/core/presentation/widgets/scrollable_container.dart';
@@ -28,12 +29,12 @@ class _NicknamePreferencesPageState extends State<NicknamePreferencesPage> {
 
   @override
   void initState() {
-    _nicknameController.text = context.read<GroupPreferencesBloc>().state.data.nickname;
-
     super.initState();
+    _nicknameController.text = context.read<GroupPreferencesBloc>().state.data.nickname;
   }
 
   void _onNextPressedHandler() {
+    dismissKeyboard(context);
     context.read<GroupPreferencesBloc>().add(GroupPreferencesEvent.setNickname(_nicknameController.text));
   }
 
@@ -45,13 +46,13 @@ class _NicknamePreferencesPageState extends State<NicknamePreferencesPage> {
     state.maybeMap(
       orElse: () => {},
       error: _onErrorHandler,
+      loading: (state) => dismissKeyboard(context),
       updated: _onUpdateHandler,
     );
   }
 
   void _onErrorHandler(GroupPreferencesState state) {
-    context.showErrorBar(
-        content: Text(state.data.error?.error.toString() ?? ''), position: FlashPosition.top);
+    context.showErrorBar(content: Text(state.data.error?.error.toString() ?? ''), position: FlashPosition.top);
   }
 
   void _onUpdateHandler(GroupPreferencesState state) {
@@ -70,58 +71,59 @@ class _NicknamePreferencesPageState extends State<NicknamePreferencesPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<GroupPreferencesBloc, GroupPreferencesState>(
-      listenWhen: (prev, cur) => context.router.current.name == NicknamePreferencesRoute.name,
+    return BlocConsumer<GroupPreferencesBloc, GroupPreferencesState>(
       listener: _onChangeListener,
-      child: GroupLessonWrap(
-        child: GroupPrefsPageWrap(
-          child: SafeArea(
-            child: MainContainer(
-              child: ScrollableContainer(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Column(
-                      children: [
-                        const GroupPrefsProgress(),
-                        const SizedBox(height: 28.0),
-                        const Text(
-                          LocalizedTexts.nicknamePreferencesQuestion,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
+      builder: (BuildContext context, GroupPreferencesState state) {
+        return GroupLessonWrap(
+          child: GroupPrefsPageWrap(
+            child: SafeArea(
+              child: MainContainer(
+                child: ScrollableContainer(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Column(
+                        children: [
+                          const GroupPrefsProgress(),
+                          const SizedBox(height: 28.0),
+                          const Text(
+                            LocalizedTexts.nicknamePreferencesQuestion,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ).tr(),
+                          const SizedBox(height: 24.0),
+                          TextField(
+                            autofocus: context.router.current.name == NicknamePreferencesRoute.name,
+                            keyboardType: TextInputType.name,
+                            onChanged: _onNicknameChangeHandler,
+                            controller: _nicknameController,
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(),
+                              hintText: LocalizedTexts.nicknamePlaceholder.tr(),
+                            ),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          OrangeButton(
+                            onPressedHandler: _nicknameController.text.isEmpty ? null : _onNextPressedHandler,
+                            child: const Text(LocalizedTexts.next).tr(),
                           ),
-                        ).tr(),
-                        const SizedBox(height: 24.0),
-                        TextField(
-                          autofocus: true,
-                          keyboardType: TextInputType.name,
-                          onChanged: _onNicknameChangeHandler,
-                          controller: _nicknameController,
-                          decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
-                            hintText: LocalizedTexts.nicknamePlaceholder.tr(),
-                          ),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        OrangeButton(
-                          onPressedHandler: _nicknameController.text.isEmpty ? null : _onNextPressedHandler,
-                          child: const Text(LocalizedTexts.next).tr(),
-                        ),
-                        const SizedBox(height: 30.0),
-                      ],
-                    ),
-                  ],
+                          const SizedBox(height: 30.0),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 
--- lib/features/account/presentation/widgets/group_prefs_page_wrap.dart
+++ lib/features/account/presentation/widgets/group_prefs_page_wrap.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loopcare_frontend/core/presentation/app_bar/blue_app_bar.dart';
 import 'package:loopcare_frontend/core/presentation/localization/localized_texts.dart';
+import 'package:loopcare_frontend/core/presentation/widgets/keyboard_listener_container.dart';
 import 'package:loopcare_frontend/features/account/application/group_preferences_bloc.dart';
 import 'package:loopcare_frontend/features/account/domain/group_prefs_mode.dart';
 import 'package:loopcare_frontend/features/nutrition/presentation/widgets/back_button_hexagon/back_button_hexagon.dart';
+
+
 
 class GroupPrefsPageWrap extends StatelessWidget {
   final Widget child;
@@ -18,23 +21,27 @@ class GroupPrefsPageWrap extends StatelessWidget {
     return BlocBuilder<GroupPreferencesBloc, GroupPreferencesState>(
       builder: (context, state) {
         if (state.data.groupPrefsMode == GroupPrefsMode.groupingLesson) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(
-                title ?? LocalizedTexts.supportGroupPreferences,
-                style: Theme.of(context).textTheme.titleMedium,
-              ).tr(),
+          return KeyboardContainerListener(
+            child: Scaffold(
+              appBar: AppBar(
+                title: Text(
+                  title ?? LocalizedTexts.supportGroupPreferences,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ).tr(),
+              ),
+              body: child,
             ),
-            body: child,
           );
         }
 
-        return Scaffold(
-          appBar: BlueAppBar(
-            title: title ?? LocalizedTexts.groupPreferences.translation,
-            leading: const BackButtonHexagon(),
+        return KeyboardContainerListener(
+          child: Scaffold(
+            appBar: BlueAppBar(
+              title: title ?? LocalizedTexts.groupPreferences.translation,
+              leading: const BackButtonHexagon(),
+            ),
+            body: child,
           ),
-          body: child,
         );
       },
     );
