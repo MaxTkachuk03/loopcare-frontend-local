4e1a6c118aa19b2f5cdd0732ff812db723842d5c
Dima Yavdoshenko <d.yavdoshenko@mobidev.biz>
2023-09-13 13:43:38 +0300

    Merge with origin

--- lib/features/account/presentation/account_page/widgets/preferences_section.dart
+++ lib/features/account/presentation/account_page/widgets/preferences_section.dart
@@ -72,20 +72,12 @@ class PreferencesSection extends StatelessWidget {
               return BlocBuilder<PhysicalActivitiesPreferencesBloc, PhysicalActivitiesPreferencesState>(
                 builder: (context, physicalActivitiesPreferencesState) {
                   return SectionItem(
-<<<<<<< HEAD
                     title: LocalizedTexts.physicalExersises,
                     subTitle: _physicalActivitiesPreferencesSubtitle(physicalActivitiesPreferencesState),
                     onPressHandler: state.unlockedFeatures.contains(UnlockedFeatureType.physicalActivities)
                         ? () => _onPhysicalActivitiesHandler(context)
                         : null,
                   );
-=======
-                      title: LocalizedTexts.physicalExersises,
-                      subTitle: _physicalActivitiesPreferencesSubtitle(physicalActivitiesPreferencesState),
-                      onPressHandler: state.unlockedFeatures.contains(UnlockedFeatureType.physicalActivities)
-                          ? () => _onPhysicalActivitiesHandler(context)
-                          : null);
->>>>>>> development
                 },
               );
             },
