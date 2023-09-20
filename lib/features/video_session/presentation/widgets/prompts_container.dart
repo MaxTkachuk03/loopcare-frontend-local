37e97798124fd9053675b0516a9a15c66cf2fdce
Artur Serdiuk <a.serdiuk@mobidev.biz>
2023-09-20 17:54:05 +0200

    [Feature] LOOPCARE-1223 Participate in Group Session - Session prompts Added sound when new prompt appears

--- lib/features/video_session/presentation/widgets/prompts_container.dart
+++ lib/features/video_session/presentation/widgets/prompts_container.dart
@@ -1,45 +1,70 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loopcare_frontend/core/presentation/themes/themes.dart';
 import 'package:loopcare_frontend/core/presentation/widgets/scrollable_container.dart';
-import 'package:loopcare_frontend/features/group_sessions/application/topics_bloc.dart';
-import 'package:loopcare_frontend/features/video_session/application/session_call_bloc.dart';
 
-class PromptsContainer extends StatelessWidget {
-  const PromptsContainer({super.key});
+class PromptsContainer extends StatefulWidget {
+  final String text;
+
+  const PromptsContainer({super.key, required this.text});
 
   static const _animationDuration = Duration(milliseconds: 300);
 
   @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<SessionCallBloc, SessionCallState>(
-      builder: (context, state) {
-        final textEvents = context.read<TopicsBloc>().state.data.textEvents;
-        final text = textEvents.lastWhereOrNull((e) => state.data.sessionTime >= e.timestamp)?.text ?? '';
+  State<PromptsContainer> createState() => _PromptsContainerState();
+}
 
-        return Container(
-          height: 100,
-          color: AppColors.white,
-          width: double.infinity,
-          padding: const EdgeInsets.all(24.0),
-          child: ScrollableContainer(
-            child: Center(
-              child: AnimatedSwitcher(
-                duration: _animationDuration,
-                child: AutoSizeText(
-                  text,
-                  key: ValueKey<String>(text),
-                  style: const TextStyle(
-                      fontSize: 18.0, fontWeight: FontWeight.w600, color: AppColors.darkGreen),
-                  textAlign: TextAlign.center,
-                ),
-              ),
+class _PromptsContainerState extends State<PromptsContainer> {
+  final AudioPlayer player = AudioPlayer();
+
+  String _text = '';
+
+  @override
+  void initState() {
+    super.initState();
+    player.setVolume(1.0);
+
+    _text = widget.text;
+  }
+
+  @override
+  void didUpdateWidget(covariant PromptsContainer oldWidget) {
+    if (oldWidget.text == widget.text) return;
+    _text = widget.text;
+
+    player.play(AssetSource('audio/sound.wav'));
+
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      color: AppColors.white,
+      width: double.infinity,
+      padding: const EdgeInsets.all(24.0),
+      child: ScrollableContainer(
+        child: Center(
+          child: AnimatedSwitcher(
+            duration: PromptsContainer._animationDuration,
+            child: AutoSizeText(
+              _text,
+              key: ValueKey<String>(widget.text),
+              style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600, color: AppColors.darkGreen),
+              textAlign: TextAlign.center,
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+
+    super.dispose();
   }
 }
