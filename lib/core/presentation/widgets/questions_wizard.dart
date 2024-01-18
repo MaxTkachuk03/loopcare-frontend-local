b5fdc8c01f1f39e9710d7fffdb0fc53ceb09a04b
Dima Yavdoshenko <d.yavdoshenko@mobidev.biz>
2024-01-18 17:32:27 +0200

    Food logging: Barcode scanner

--- lib/core/presentation/widgets/questions_wizard.dart
+++ lib/core/presentation/widgets/questions_wizard.dart
@@ -1,3 +1,5 @@
+
+// TODO: Dead Code
 // import 'package:easy_localization/easy_localization.dart';
 // import 'package:flutter/material.dart';
 // import 'package:loopcare_frontend/core/presentation/localization/localized_texts.dart';
