2c1a09c27c5b63255873930d2a4930880372bd65
Dima Yavdoshenko <d.yavdoshenko@mobidev.biz>
2024-01-31 18:42:51 +0200

    Modify GenderPrefsGuard for using args

--- lib/core/presentation/routes/gender_prefs_guard.dart
+++ lib/core/presentation/routes/gender_prefs_guard.dart
@@ -1,5 +1,5 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:loopcare_frontend/core/presentation/routes/app_router.dart';
+import 'package:loopcare_frontend/core/presentation/routes/app_router.gr.dart';
 import 'package:loopcare_frontend/features/authentication/application/authentication_cubit.dart';
 import 'package:loopcare_frontend/features/onboarding/onboarding_physical/domain/sex_type.dart';
 
@@ -15,7 +15,7 @@ class GenderPrefsGuard extends AutoRouteGuard {
     if (gender == SexType.female || gender == SexType.male) {
       resolver.next(true);
     } else {
-      router.replaceNamed(AppRoutes.timezone);
+      router.push(TimezonePreferencesRoute(fromLessonComplete: resolver.route.args.fromLessonComplete));
     }
   }
 }
