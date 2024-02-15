57f8213f7b3ad20fa5447df5d6ad30c49bd2f7d5
Dima Yavdoshenko <d.yavdoshenko@mobidev.biz>
2024-02-15 16:53:42 +0200

    Modify height conversion utils for use double instead int

--- lib/features/onboarding/onboarding_physical/utils/height_conversion_utils.dart
+++ lib/features/onboarding/onboarding_physical/utils/height_conversion_utils.dart
@@ -5,27 +5,6 @@ class HeightConversionUtils {
   static double cmInFoot = 30.48;
   static double cmInInch = 2.54;
 
-  static int convertCMtoFT(int lengthInCm) => lengthInCm.floor() / cmInInch ~/ inchesInFoot;
-
-  static double convertCMtoIN(int lengthInCm) => lengthInCm.floor() * inchInCm;
-
-  static int convertCMtoFtIn(int lengthInCm) {
-    var foot = convertCMtoFT(lengthInCm);
-    var inches = (lengthInCm / cmInInch).ceil() - convertFTtoIN(foot);
-    return inches.round();
-  }
-
-  static int convertINtoFT(int inches) => inches ~/ inchesInFoot;
-
-  static double convertFTtoCM(int foot) => foot * cmInFoot;
-
-  static int convertFTtoIN(int foot) => foot * inchesInFoot;
-
-  static double convertINtoCM(int inches) => inches * cmInInch;
-
-  static int convertFeetAndInchesToCM(int foot, int inches) =>
-      (convertFTtoCM(foot) + convertINtoCM(inches)).round();
-
   static double doubleConvertFeetAndInchesToCM(double foot, double inches) =>
       doubleConvertFTtoCM(foot) + doubleConvertINtoCM(inches);
 
