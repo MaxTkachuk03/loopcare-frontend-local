f2e5756e415b6304bdac0b53c4cabdacb8adaca5
Artur Serdiuk <a.serdiuk@mobidev.biz>
2024-02-19 13:21:06 +0100

    [Fix] LOOPCARE-2237 PhysicalActivities feature does not unlock when user complete the lesson that unlockPhysicalActivities

--- lib/features/education/presentation/lesson_page/lesson_page.dart
+++ lib/features/education/presentation/lesson_page/lesson_page.dart
@@ -70,6 +70,7 @@ class _LessonPageState extends State<LessonPage> {
 
       if (extraAction == ExtraActionTypes.unlockPhysicalActivities &&
           !unlockedFeatures.contains(UnlockedFeatureType.physicalActivities)) {
+        context.read<AuthenticationCubit>().unlockFeature(UnlockedFeatureType.physicalActivities);
         context.router.pushNamed(AppRoutes.physicalActivitiesPreferences);
 
         return;
