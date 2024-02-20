b8ff4bd67fdf8291e119a2a3f078b0dcd4affd45
Antonina Lehka <a.lehka@mobidev.biz>
2024-02-20 21:26:10 +0100

    after merge fixes

--- lib/features/mental_health/presentation/widgets/who5_result_text.dart
+++ lib/features/mental_health/presentation/widgets/who5_result_text.dart
@@ -3,6 +3,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loopcare_frontend/core/domain/url_constants.dart';
+import 'package:loopcare_frontend/core/infrastructure/services/firebase_event_service.dart';
 import 'package:loopcare_frontend/core/presentation/localization/localized_texts.dart';
 import 'package:loopcare_frontend/core/presentation/themes/themes.dart';
 import 'package:loopcare_frontend/core/presentation/utils/build_context_extensions.dart';
@@ -22,10 +23,21 @@ class WHO5ResultText extends StatelessWidget {
 
         if (currentTestType == null) return const SizedBox.shrink();
 
-        final interpretation = state.data.results[currentTestType]?.interpretation;
+        final currentResult = state.data.results[currentTestType];
+        final interpretation = currentResult?.interpretation;
         final text = interpretation == InterpretationType.minimal
             ? LocalizedTexts.who5ResultTestMinimal.tr()
             : LocalizedTexts.who5ResultTestHigh.tr();
+
+        AnalyticsEventService.instance.logEvent(
+          FirebaseEvents.userMentalHealthTest,
+          parameters: {
+            CustomDefinitions.testType: currentTestType.name,
+            CustomDefinitions.itemInterpretation: interpretation?.name ?? '',
+            CustomDefinitions.totalScore: currentResult?.totalScore ?? '',
+            CustomDefinitions.exclusion: interpretation == InterpretationType.high ? 'false' : 'true'
+          },
+        );
 
         return RichText(
           text: TextSpan(children: [
