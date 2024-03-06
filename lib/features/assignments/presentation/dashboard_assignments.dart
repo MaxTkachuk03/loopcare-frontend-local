64ea3107e9e7f21aceba3d9e06ce5c5fe72a5e50
Artur Serdiuk <a.serdiuk@mobidev.biz>
2024-03-06 06:59:40 +0100

    [Fix] LOOPCARE-2266 Clickable area of assignment widget is too small

--- lib/features/assignments/presentation/dashboard_assignments.dart
+++ lib/features/assignments/presentation/dashboard_assignments.dart
@@ -22,80 +22,82 @@ class DashboardAssignments extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(top: 8.0, bottom: 24.0, right: 16.0, left: 16.0),
       decoration: const BoxDecoration(
         color: AppColors.white,
         borderRadius: BorderRadius.all(Radius.circular(8)),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 8.0, bottom: 16.0, right: 16.0, left: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            InkWell(
+              onTap: () => context.router.push(const MyAssignmentsRoute()),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  AppIcons.customDashboardAssignments,
-                  const SizedBox(width: 24.0),
-                  CustomText.bitter600(
-                    LocalizedTexts.assignments.tr(),
-                    style: context.textTheme.headlineSmall,
+                  Row(
+                    children: [
+                      AppIcons.customDashboardAssignments,
+                      const SizedBox(width: 24.0),
+                      CustomText.bitter600(
+                        LocalizedTexts.assignments.tr(),
+                        style: context.textTheme.headlineSmall,
+                      ),
+                    ],
+                  ),
+                  const ImageIcon(
+                    AppIcons.arrow,
+                    color: AppColors.blueDarker,
                   ),
                 ],
               ),
-              GestureDetector(
-                onTap: () => context.router.push(const MyAssignmentsRoute()),
-                child: const ImageIcon(
-                  AppIcons.arrow,
-                  color: AppColors.blueDarker,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 8.0),
-          const Divider(color: AppColors.blueOffRegular),
-          BlocBuilder<AssignmentsBloc, AssignmentsState>(
-            builder: (context, state) {
-              return state.maybeMap(
-                loading: (_) => const SizedBox(height: 100, child: Loader()),
-                error: (errorState) {
-                  final error = errorState.data.error;
+            ),
+            const SizedBox(height: 8.0),
+            const Divider(color: AppColors.blueOffRegular),
+            BlocBuilder<AssignmentsBloc, AssignmentsState>(
+              builder: (context, state) {
+                return state.maybeMap(
+                  loading: (_) => const SizedBox(height: 100, child: Loader()),
+                  error: (errorState) {
+                    final error = errorState.data.error;
 
-                  return ErrorScreen(
-                    error: error!,
-                    onButtonPressed: () => context.read<AssignmentsBloc>().add(
-                          AssignmentsEvent.getAllLessonQuestions(
-                            date.beginDay,
-                            date.endDay,
+                    return ErrorScreen(
+                      error: error!,
+                      onButtonPressed: () => context.read<AssignmentsBloc>().add(
+                            AssignmentsEvent.getAllLessonQuestions(
+                              date.beginDay,
+                              date.endDay,
+                            ),
                           ),
-                        ),
-                  );
-                },
-                orElse: () {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      if (state.data.questionsForCurrentWeek(date).isEmpty)
-                        CustomText.w400(
-                          LocalizedTexts.allAssignmentsCompleted.tr(),
-                          style: context.textTheme.bodyMedium,
-                        ),
-                      if (state.data.questionsForCurrentWeek(date).isNotEmpty)
-                        ThisWeekAssignments(
-                          weekQuestions: state.data.uniqueLessonsQuestions(
-                            state.data.openedQuestionsForCurrentWeek(date),
+                    );
+                  },
+                  orElse: () {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (state.data.questionsForCurrentWeek(date).isEmpty)
+                          CustomText.w400(
+                            LocalizedTexts.allAssignmentsCompleted.tr(),
+                            style: context.textTheme.bodyMedium,
                           ),
-                          todayQuestions: state.data.uniqueLessonsQuestions(
-                            state.data.doneTodayQuestions(date),
+                        if (state.data.questionsForCurrentWeek(date).isNotEmpty)
+                          ThisWeekAssignments(
+                            weekQuestions: state.data.uniqueLessonsQuestions(
+                              state.data.openedQuestionsForCurrentWeek(date),
+                            ),
+                            todayQuestions: state.data.uniqueLessonsQuestions(
+                              state.data.doneTodayQuestions(date),
+                            ),
                           ),
-                        ),
-                    ],
-                  );
-                },
-              );
-            },
-          ),
-        ],
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
