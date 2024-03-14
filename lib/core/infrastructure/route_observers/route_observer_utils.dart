ae64fe2479cf0fe1b7e51c721103ff8f0c15b795
Artur Serdiuk <a.serdiuk@mobidev.biz>
2024-03-14 10:15:36 +0000

    [Fix] LOOPCARE-3240 The audio continue play when the lesson content closes and...

---
+++ lib/core/infrastructure/route_observers/route_observer_utils.dart
@@ -0,0 +1,8 @@
+import 'package:auto_route/auto_route.dart';
+
+/// Route change observer
+/// Can be used through [AutoRouteAware] mixin to add any custom behaviour on route change
+
+class RouteObserverUtils extends AutoRouteObserver {
+  RouteObserverUtils();
+}
