db7888344b9cc533c8dbfc1aed056a9e5f9beed1
Artur Serdiuk <a.serdiuk@mobidev.biz>
2024-04-11 16:43:44 +0200

    Added goal review handler

--- lib/features/dashboard/presentation/widgets/smart_goals/widgets/dashboard_weekly_goals.dart
+++ lib/features/dashboard/presentation/widgets/smart_goals/widgets/dashboard_weekly_goals.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,6 +6,7 @@ import 'package:loopcare_frontend/core/presentation/buttons/custom_elevated_butt
 import 'package:loopcare_frontend/core/presentation/error/error_screen.dart';
 import 'package:loopcare_frontend/core/presentation/loader/loader.dart';
 import 'package:loopcare_frontend/core/presentation/localization/localized_texts.dart';
+import 'package:loopcare_frontend/core/presentation/routes/app_router.gr.dart';
 import 'package:loopcare_frontend/features/dashboard/presentation/widgets/smart_goals/widgets/dashboard_weekly_goal_item.dart';
 import 'package:loopcare_frontend/features/dashboard/presentation/widgets/smart_goals/widgets/weekly_goals_empty_state.dart';
 import 'package:loopcare_frontend/features/smart_goals/application/smart_goals_bloc.dart';
@@ -25,6 +27,16 @@ class _DashboardWeeklyGoalsState extends State<DashboardWeeklyGoals> {
 
   void _onErrorRetryHandler() => context.read<SmartGoalsBloc>().add(const SmartGoalsEvent.getWeeklyGoals());
 
+  void _onQuickReviewHandler() {
+    final goal = context.read<SmartGoalsBloc>().state.data.firstGoalForReview;
+
+    if (goal == null) return;
+
+    final isLast = context.read<SmartGoalsBloc>().state.data.isLastGoalInSession(goal);
+
+    context.router.push(GoalReviewRoute(goal: goal, isLast: isLast));
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SmartGoalsBloc, SmartGoalsState>(builder: (context, state) {
@@ -32,7 +44,9 @@ class _DashboardWeeklyGoalsState extends State<DashboardWeeklyGoals> {
           loading: (_) => const Loader(),
           error: (s) => ErrorScreen(error: s.data.error!, onButtonPressed: _onErrorRetryHandler),
           orElse: () {
-            if (!state.data.hasWeeklyGoals || !state.data.hasReviewDelay) return const WeeklyGoalsEmptyState();
+            if (!state.data.hasWeeklyGoals || !state.data.hasReviewDelay) {
+              return const WeeklyGoalsEmptyState();
+            }
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 6.0),
               child: Column(
@@ -53,16 +67,12 @@ class _DashboardWeeklyGoalsState extends State<DashboardWeeklyGoals> {
                   if (state.data.hasQuickReviewWeeklyGoals)
                     CustomElevatedButton.greenSmall(
                       label: LocalizedTexts.weeklyQuickReview.tr(),
-                      onPressed: () => _onQuickReviewHandler(context),
+                      onPressed: _onQuickReviewHandler,
                     ),
                 ],
               ),
             );
           });
     });
-  }
-
-  _onQuickReviewHandler(BuildContext context) {
-    // push route Quick review
   }
 }
