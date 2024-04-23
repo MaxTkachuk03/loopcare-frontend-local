7844ec98bfcad3d59d241745429b58e734b97fe1
klimkin <a.klimkin@mobidev.biz>
2024-04-23 13:00:36 +0300

    LOOPCARE-2613 [FE] Make user ID unique based on enviroment (USA or EU) in Firebase and in c.io

--- lib/core/presentation/localization/localization_constants.dart
+++ lib/core/presentation/localization/localization_constants.dart
@@ -37,6 +37,8 @@ class LocalizationConstants {
 
   static get useUsServer => _usCodes.contains(Platform.localeName.substring(3));
 
+  static String get serverCountryCode => useUsServer ? 'US' : 'EU';
+
   // TODO comment other languages before get translations
   static const List<Locale> supportedLocales = [
     localeEnglish,
