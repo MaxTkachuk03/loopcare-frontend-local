3011282c02021e040ea0547f5bd284cbfa5b41fb
klimkin <a.klimkin@mobidev.biz>
2024-05-13 18:08:35 +0300

    LOOPCARE-2701 [FE] Add security storage (for storing users data)

--- lib/core/presentation/auth_token_manager_shared_prefs/auth_token_manager_shared_prefs.dart
+++ lib/core/presentation/auth_token_manager_shared_prefs/auth_token_manager_shared_prefs.dart
@@ -1,21 +1,25 @@
 import 'package:injectable/injectable.dart';
 import 'package:loopcare_frontend/core/application/auth_token_manager.dart';
 import 'package:loopcare_frontend/core/application/auth_token_service.dart';
+import 'package:loopcare_frontend/core/infrastructure/services/secure_storage/secure_storage_service.dart';
 import 'package:loopcare_frontend/core/infrastructure/services/shared_storage/shared_storage_service.dart';
 
+// TODO: remove parameters
 const _accessTokenKey = 'access_token';
 const _refreshTokenKey = 'refresh_token';
 
 @Singleton(as: AuthTokenManager)
 class TokenManagerSharedPrefs extends AuthTokenManager {
+  final SecureStorageService _secureStorage;
+  // TODO: remove dependency
   final SharedStorageService _sharedPreferences;
   final AuthTokenService authTokenService;
 
   final Set<AccessTokenListener> _listeners = {};
 
-  TokenManagerSharedPrefs(this.authTokenService, this._sharedPreferences);
+  TokenManagerSharedPrefs(this.authTokenService, this._secureStorage, this._sharedPreferences);
 
-  _updateListener(String? token) {
+  void _updateListener(String? token) {
     for (var l in _listeners) {
       l(token);
     }
@@ -23,23 +27,37 @@ class TokenManagerSharedPrefs extends AuthTokenManager {
 
   @override
   Future<String?> getAccessToken() async {
-    return _sharedPreferences.getString(_accessTokenKey);
+    final accessTokenFromShared = _sharedPreferences.getString(_accessTokenKey);
+    if (accessTokenFromShared != null) {
+      _sharedPreferences.remove(_accessTokenKey);
+      await setAccessToken(accessTokenFromShared);
+    }
+
+    return await _secureStorage.getAccessToken();
   }
 
   @override
   Future<String?> getRefreshToken() async {
-    return _sharedPreferences.getString(_refreshTokenKey);
+    final refreshTokenFromShared = _sharedPreferences.getString(_refreshTokenKey);
+    if (refreshTokenFromShared != null) {
+      _sharedPreferences.remove(_refreshTokenKey);
+      await setRefreshToken(refreshTokenFromShared);
+    }
+
+    return _secureStorage.getRefreshToken();
   }
 
   @override
   Future<void> removeAccessToken() async {
     _sharedPreferences.remove(_accessTokenKey);
+    _secureStorage.removeAccessToken();
     _updateListener(null);
   }
 
   @override
   Future<void> removeRefreshToken() async {
     _sharedPreferences.remove(_refreshTokenKey);
+    _secureStorage.removeRefreshToken();
   }
 
   @override
@@ -85,21 +103,19 @@ class TokenManagerSharedPrefs extends AuthTokenManager {
 
   @override
   Future<void> setAccessToken(String token) async {
-    _sharedPreferences.setString(_accessTokenKey, token);
+    _secureStorage.setAccessToken(token);
     _updateListener(token);
   }
 
   @override
   Future<void> setRefreshToken(String token) async {
-    _sharedPreferences.setString(_refreshTokenKey, token);
+    _secureStorage.setRefreshToken(token);
   }
 
   @override
   AccessTokenSubscription addListener(AccessTokenListener listener) {
     _listeners.add(listener);
 
-    return () {
-      _listeners.remove(listener);
-    };
+    return () => _listeners.remove(listener);
   }
 }
