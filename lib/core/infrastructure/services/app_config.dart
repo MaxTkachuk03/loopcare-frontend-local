eb089e7405c2c24e328b78dd8561638f918b3f0e
Artur Serdiuk <a.serdiuk@mobidev.biz>
2024-05-23 13:50:01 +0000

    Revert "Merge branch 'feature/LOOPCARE-2749' into 'development'"
    This reverts merge request !775

--- lib/core/infrastructure/services/app_config.dart
+++ lib/core/infrastructure/services/app_config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:injectable/injectable.dart';
-import 'package:loopcare_frontend/core/infrastructure/services/country_code_service/country_code_service.dart';
+import 'package:loopcare_frontend/core/presentation/localization/localization_constants.dart';
 
 final GlobalKey<NavigatorState> kNavigatorKey = GlobalKey<NavigatorState>();
 
@@ -12,9 +12,9 @@ class AppConfig {
   String get projectName => 'LeanOnMe';
 
   String get baseUrl =>
-      CountryCodeService.instance.useUsServer ? dotenv.env['BASE_URL'] ?? "" : dotenv.env['BASE_URL_EU'] ?? "";
+      LocalizationConstants.useUsServer ? dotenv.env['BASE_URL'] ?? "" : dotenv.env['BASE_URL_EU'] ?? "";
 
-  String get region => CountryCodeService.instance.countryCode;
+  String get region => LocalizationConstants.regionCode;
 
   String get baseHost => Uri.parse(baseUrl).host;
 
