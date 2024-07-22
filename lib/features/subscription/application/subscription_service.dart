fc86ec984e88bdc1cbbddb2f339af82a7bb1c285
Antonina Lehka <a.lehka@mobidev.biz>
2024-07-22 19:05:56 +0300

    [MERGE] merged with uat

--- lib/features/subscription/application/subscription_service.dart
+++ lib/features/subscription/application/subscription_service.dart
@@ -2,7 +2,6 @@
 
 import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:in_app_purchase_storekit/store_kit_wrappers.dart';
 import 'package:injectable/injectable.dart';
