3cc30f089bb1983f224a81c9a88599af52f1395b
klimkin <a.klimkin@mobidev.biz>
2024-08-21 14:04:09 +0300

    fix serStatesModelAdapter enum parser

--- lib/core/infrastructure/services/user_states_service/src/api_user_states_repository/user_states_model_adapter.dart
+++ lib/core/infrastructure/services/user_states_service/src/api_user_states_repository/user_states_model_adapter.dart
@@ -1,5 +1,6 @@
 import 'package:hive/hive.dart';
 import 'package:loopcare_frontend/core/infrastructure/services/user_states_service/src/user_states_model/user_states_model.dart';
+import 'package:loopcare_frontend/features/account/presentation/buddy_page/application/buddy_status.dart';
 
 class UserStatesModelAdapter extends TypeAdapter<UserStatesModel> {
   @override
@@ -8,7 +9,8 @@ class UserStatesModelAdapter extends TypeAdapter<UserStatesModel> {
   @override
   UserStatesModel read(BinaryReader reader) {
     final id = reader.read() as int;
-    final buddyStatus = reader.read() as String?;
+    final buddyStatusIndex = reader.read() as int?;
+    final buddyStatus = buddyStatusIndex != null ? BuddyStatus.values[buddyStatusIndex] : null;
 
     return UserStatesModel(id: id, buddyStatus: buddyStatus);
   }
@@ -17,6 +19,6 @@ class UserStatesModelAdapter extends TypeAdapter<UserStatesModel> {
   void write(BinaryWriter writer, UserStatesModel obj) {
     writer
       ..write(obj.id)
-      ..write(obj.buddyStatus);
+      ..write(obj.buddyStatus?.index);
   }
 }
