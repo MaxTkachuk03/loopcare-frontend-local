ffbc44421cdc74bd7fc69745eb55e4f12a36db70
Artur Serdiuk <a.serdiuk@mobidev.biz>
2024-08-29 13:10:06 +0200

    [Fix] LOOPCARE-3131 Crash AppsflyerSdkPlugin Added error handling to the appFlyer initSDK method

--- lib/core/infrastructure/services/apps_flyer_service.dart
+++ lib/core/infrastructure/services/apps_flyer_service.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:appsflyer_sdk/appsflyer_sdk.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:loopcare_frontend/build_type.dart';
 import 'package:loopcare_frontend/core/domain/analytics/apps_flyer/apps_flyer_events.dart';
@@ -20,38 +21,55 @@ class AppsFlyerService {
   static AppsflyerSdk appsflyerSdk = AppsflyerSdk(appsFlyerOptions);
 
   static Future<void> start() async {
-
-    await appsflyerSdk.initSdk(
-      registerConversionDataCallback: true,
-      registerOnAppOpenAttributionCallback: true,
-      registerOnDeepLinkingCallback: true,
-    );
+    try {
+      await appsflyerSdk.initSdk(
+        registerConversionDataCallback: true,
+        registerOnAppOpenAttributionCallback: true,
+        registerOnDeepLinkingCallback: true,
+      );
+    } catch (e) {
+      FirebaseCrashlytics.instance.recordError(
+        'Error init appFlyer SDK ${e.toString()}',
+        null,
+        fatal: true,
+      );
+    }
 
     if (Platform.isIOS) {
-      TrackingStatus status = await AppTrackingTransparency.trackingAuthorizationStatus;
+      TrackingStatus status =
+          await AppTrackingTransparency.trackingAuthorizationStatus;
 
       final forGdpr = AppsFlyerConsent.forGDPRUser(
         hasConsentForDataUsage: true,
-        hasConsentForAdsPersonalization: status == TrackingStatus.authorized ? true : false,
+        hasConsentForAdsPersonalization:
+            status == TrackingStatus.authorized ? true : false,
       );
 
       appsflyerSdk.setConsentData(forGdpr);
     }
 
     appsflyerSdk.onAppOpenAttribution((res) {
-      appsflyerSdk.logEvent(AppsFlyerEvents.onAppOpenAttribution, {"res": res.toString()});
+      appsflyerSdk.logEvent(
+          AppsFlyerEvents.onAppOpenAttribution, {"res": res.toString()});
     });
 
     appsflyerSdk.onInstallConversionData((res) {
-      appsflyerSdk.logEvent(AppsFlyerEvents.onInstallConversionData, {"res": res.toString()});
+      appsflyerSdk.logEvent(
+          AppsFlyerEvents.onInstallConversionData, {"res": res.toString()});
     });
 
     appsflyerSdk.startSDK(
       onSuccess: () {},
       onError: (int errorCode, String errorMessage) {
+        FirebaseCrashlytics.instance.recordError(
+          'Error start appFlyer SDK $errorCode - $errorMessage',
+          null,
+          fatal: true,
+        );
+
         MixpanelEventService.instance.track(
           AppMixpanelEvents.appflyerSdkStartError,
-          {'error': "code $errorCode - $errorMessage"},
+          {'error start appsFlyer SDK': "code $errorCode - $errorMessage"},
         );
       },
     );
