8de7a988110f71cb610b650559d3499a8c6cd3e3
klimkin <a.klimkin@mobidev.biz>
2024-08-30 08:22:38 +0300

    LOOPCARE-3147 There is no notification by socket on the bottom navigation bar and user profile when buddy accept the invite

--- lib/features/authentication/application/authentication_bloc.dart
+++ lib/features/authentication/application/authentication_bloc.dart
@@ -680,7 +680,7 @@ class AuthenticationBloc extends HydratedBloc<AuthenticationEvent, Authenticatio
           emit(AuthenticationState.needUpdatePolicies(state.data.copyWith(account: account)));
         } else {
           final showBuddyNews = _statesService.buddyStatus != account.buddyState &&
-              account.buddyState.isRejectedOrLeft;
+              !account.buddyState.isInvited;
 
           if (showBuddyNews) {
             _syncService.showProfileNotificationBadge();
