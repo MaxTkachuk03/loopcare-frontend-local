4dd5e5c4aa3a4303a99e6d4f4aba3ecb7dd7e64f
klimkin <a.klimkin@mobidev.biz>
2024-09-03 14:13:03 +0300

    fix import

--- lib/features/river/presentation/river_page.dart
+++ lib/features/river/presentation/river_page.dart
@@ -6,10 +6,9 @@ import 'package:loopcare_frontend/core/presentation/error/error_screen.dart';
 import 'package:loopcare_frontend/core/presentation/routes/app_router.dart';
 import 'package:loopcare_frontend/core/presentation/scaffold/custom_scaffold.dart';
 import 'package:loopcare_frontend/core/presentation/scale_gesture_detector/scale_gesture_detector.dart';
+import 'package:loopcare_frontend/features/reflections/application/reflections_bloc.dart';
 import 'package:loopcare_frontend/features/river/application/river_bloc.dart';
 import 'package:loopcare_frontend/features/river/presentation/river_page_widgets/river_module_view.dart';
-
-import '../../reflections/application/reflections_bloc.dart';
 
 @RoutePage()
 class RiverPage extends StatefulWidget {
