8e75b57c8479dc004a7bbb4e9de39c1f31387b62
Artur Serdiuk <a.serdiuk@mobidev.biz>
2024-09-04 09:32:06 +0200

    [Fix] LOOPCARE-3173 There is no information about invited buddy when user invite buddy and open again buddy preference flow Added invited state after user invite buddy

--- lib/features/account/presentation/buddy_page/application/buddy_bloc.dart
+++ lib/features/account/presentation/buddy_page/application/buddy_bloc.dart
@@ -106,7 +106,7 @@ class BuddyBloc extends Bloc<BuddyEvent, BuddyState> {
 
     response.fold(
       (l) => emit(BuddyState.error(state.data.copyWith(error: l, isLoading: false))),
-      (r) => emit(BuddyState.gotBuddy(state.data.copyWith(isLoading: false))),
+      (r) => emit(BuddyState.invited(state.data.copyWith(isLoading: false))),
     );
   }
 
