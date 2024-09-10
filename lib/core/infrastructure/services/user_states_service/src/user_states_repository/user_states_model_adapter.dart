1abadb59103ab2b3d37827eda19c5cdae2c576d3
klimkin <a.klimkin@mobidev.biz>
2024-09-10 15:07:03 +0300

    Fix User States Model storing

--- lib/core/infrastructure/services/user_states_service/src/user_states_repository/user_states_model_adapter.dart
+++ lib/core/infrastructure/services/user_states_service/src/user_states_repository/user_states_model_adapter.dart
@@ -1,7 +1,6 @@
 import 'package:hive/hive.dart';
 import 'package:loopcare_frontend/core/infrastructure/hive_service/hive_constants.dart';
 import 'package:loopcare_frontend/core/infrastructure/services/user_states_service/src/user_states_model/user_states_model.dart';
-import 'package:loopcare_frontend/features/account/presentation/buddy_page/application/buddy_status.dart';
 
 class UserStatesModelAdapter extends TypeAdapter<UserStatesModel> {
   @override
@@ -9,17 +8,13 @@ class UserStatesModelAdapter extends TypeAdapter<UserStatesModel> {
 
   @override
   UserStatesModel read(BinaryReader reader) {
-    final id = reader.read() as int;
-    final buddyStatusIndex = reader.read() as int?;
-    final buddyStatus = buddyStatusIndex != null ? BuddyStatus.values[buddyStatusIndex] : null;
+    final json = reader.readMap() as Map<String, dynamic>;
 
-    return UserStatesModel(id: id, buddyStatus: buddyStatus);
+    return UserStatesModel.fromJson(json);
   }
 
   @override
   void write(BinaryWriter writer, UserStatesModel obj) {
-    writer
-      ..write(obj.id)
-      ..write(obj.buddyStatus?.index);
+    writer.writeMap(obj.toJson(), writeLength: false);
   }
 }
