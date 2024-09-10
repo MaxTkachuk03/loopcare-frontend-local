11d12465ee3fee6aaccb774c1231b6bbf4ee12b7
Artur Serdiuk <a.serdiuk@mobidev.biz>
2024-09-10 10:56:03 +0200

    Fixed issues after merge

--- lib/core/application/localization/crowdin_localizations.dart
+++ lib/core/application/localization/crowdin_localizations.dart
@@ -1,17 +1,12 @@
-import 'dart:convert';
-
 import 'app_localizations.dart';
-
 import 'package:crowdin_sdk/crowdin_sdk.dart';
-
 import 'package:flutter/widgets.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 class CrowdinLocalization extends AppLocalizations {
   final AppLocalizations _fallbackTexts;
   
-  CrowdinLocalization(String locale, AppLocalizations fallbackTexts) : _fallbackTexts = fallbackTexts, super(locale);
+  CrowdinLocalization(super.locale, AppLocalizations fallbackTexts) : _fallbackTexts = fallbackTexts;
 
   static const LocalizationsDelegate<AppLocalizations> delegate = _CrowdinLocalizationsDelegate();
 
