cf07a149be147f759a7b5ced8e604006d2198849
Viktoriia Cherkashyna <viktoriia.che@cgsteam.io>
2025-01-21 09:27:15 +0100

    fix: Add status for card, sort reflections by status, rename card

---
+++ lib/core/presentation/practice_lesson_list/practice_lesson_card_status_types.dart
@@ -0,0 +1,1 @@
+enum PracticeLessonCardStatusTypes { locked, unlocked, completed }
---
+++ lib/features/reflections/presentation/reflections_page.dart
@@ -0,0 +1,100 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:loopcare_frontend/core/presentation/app_bar/custom_app_bar.dart';
+import 'package:loopcare_frontend/core/presentation/buttons/custom_filled_icon_button.dart';
+import 'package:loopcare_frontend/core/presentation/custom_safe_area.dart';
+import 'package:loopcare_frontend/core/presentation/loader/loader.dart';
+import 'package:loopcare_frontend/core/presentation/practice_lesson_list/practice_lesson_card.dart';
+import 'package:loopcare_frontend/core/presentation/routes/app_router.gr.dart';
+import 'package:loopcare_frontend/core/presentation/scaffold/custom_scaffold.dart';
+import 'package:loopcare_frontend/core/presentation/themes/themes.dart';
+import 'package:loopcare_frontend/core/presentation/widgets/main_container.dart';
+import 'package:loopcare_frontend/core/presentation/widgets/scrollable_container.dart';
+import 'package:loopcare_frontend/features/reflections/application/reflections_bloc.dart';
+import 'package:loopcare_frontend/features/reflections/domain/reflection.dart';
+import 'package:loopcare_frontend/localization/service/localization_extension.dart';
+import 'package:loopcare_frontend/localization/service/localized_texts.dart';
+
+import '../../../core/presentation/practice_lesson_list/practice_lesson_card_status_types.dart';
+
+@RoutePage()
+class ReflectionsPage extends StatefulWidget {
+  const ReflectionsPage({super.key});
+
+  @override
+  State<ReflectionsPage> createState() => _ReflectionsPageState();
+}
+
+class _ReflectionsPageState extends State<ReflectionsPage> {
+  void _onItemPressedHandler(BuildContext context, Reflection reflectionItem) => context.router
+      .push(ReflectionsIntroRoute(reflectionItem: reflectionItem, fromDashboard: false));
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      color: AppColors.white,
+      appBar: CustomAppBar.petrol(
+        title: LocalizedTexts.reflections.tr(),
+        leading: CustomFilledIconButton.leadingPetrolLighter(),
+      ),
+      body: CustomSafeArea(
+        child: ScrollableContainer(
+          child: MainContainer(
+            child: BlocBuilder<ReflectionsBloc, ReflectionsState>(
+              builder: (context, state) {
+                final sortedReflections = List<Reflection>.from(state.data.reflections)
+                  ..sort((a, b) {
+                    int getStatusPriority(Reflection reflection) {
+                      if (reflection.unlockedAt != null && reflection.completedAt != null) {
+                        return 0; // Completed
+                      } else if (reflection.unlockedAt != null && reflection.completedAt == null) {
+                        return 1; // Unlocked
+                      } else {
+                        return 2; // Locked
+                      }
+                    }
+
+                    return getStatusPriority(a).compareTo(getStatusPriority(b));
+                  });
+
+                return state.maybeMap(
+                  loading: (_) => const Loader(),
+                  orElse: () {
+                    return ListView.builder(
+                        padding: const EdgeInsets.only(top: 28),
+                        physics: const ScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: state.data.reflections.length,
+                        itemBuilder: (context, index) {
+                          final reflection = sortedReflections[index];
+                          final cardStatus =
+                              reflection.unlockedAt == null && reflection.completedAt == null
+                                  ? PracticeLessonCardStatusTypes.locked
+                                  : reflection.unlockedAt != null && reflection.completedAt == null
+                                      ? PracticeLessonCardStatusTypes.unlocked
+                                      : PracticeLessonCardStatusTypes.completed;
+
+                          return PracticeLessonCard(
+                            key: ValueKey(reflection.id),
+                            url: reflection.image,
+                            text: reflection.title,
+                            buttonText: LocalizedTexts.start.tr(),
+                            status: cardStatus.name,
+                            onPressed: cardStatus == PracticeLessonCardStatusTypes.locked
+                                ? null
+                                : () {
+                                    _onItemPressedHandler(context, reflection);
+                                  },
+                          );
+                        });
+                  },
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
